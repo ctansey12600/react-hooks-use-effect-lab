d951fc963745326dc6a7d50308738a6554a793a2
caseytansey <ctansey12600@outlook.com>
2021-11-29 13:08:53 -0600

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,21 @@ import Question from "./Question";
 import quiz from "../data/quiz";
 
 function App() {
+  //This state holds an array with content of
+  // {id: 1, prompt: 'What special prop should always be included for lists of elements?'
+  // , answers: Array(4), correctIndex: 2}
   const [questions, setQuestions] = useState(quiz);
+  //This needs to go up by 1 each time that the answer is correct so that the answer changes
   const [currentQuestionId, setCurrentQuestion] = useState(1);
+  //This needs to increase by 1 each time the answer is correct
   const [score, setScore] = useState(0);
+  //This line takes in the array of question and finds the first id that matches the state of 1
   const currentQuestion = questions.find((q) => q.id === currentQuestionId);
+
+  console.log("Questions:", questions)
+  console.log("CurrentQuestionId:", currentQuestionId)
+  console.log("Score:", score)
+  console.log("Current Question:", currentQuestion)
 
   function handleQuestionAnswered(correct) {
     if (currentQuestionId < questions.length) {
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,12 +1,38 @@
-import React, { useState } from "react";
+//When the Question component renders, create a side effect using 
+//useEffect and use setTimeout to run a callback function after 1 second.
+
+// Inside the callback function for setTimeout, use the setTimeRemaining 
+//function to decrease the amount of time remaining by 1 every 1 second.
+
+// When timeRemaining hits 0, do the following:
+
+// reset timeRemaining back to 10 seconds, so our next question will have 
+// a fresh timer; and call the onAnswered callback prop with a value of false 
+// (onAnswered(false)), to trigger some behavior in the App component.
+
+// You should also use the cleanup function for useEffect to clean up 
+// after the timeout function.
+
+import React, { useState, useEffect } from 'react';
+
+//question prop is an object that holds an id, prompt, answers, correectIndex(postion of answer)
+//onAnswered takes in an agruement only if the answer is correct
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  useEffect(() => {
+    if(timeRemaining > 0){
+      const timer = setTimeout(() => {setTimeRemaining(() => timeRemaining - 1)}, 1000)
+      return () => clearTimeout(timer)
+    }else{
+      setTimeRemaining(10)
+      onAnswered(false)
+    }
+  }, [timeRemaining]);
 
   function handleAnswer(isCorrect) {
-    setTimeRemaining(10);
+    setTimeRemaining(10)
     onAnswered(isCorrect);
   }
 
@@ -16,7 +42,10 @@ function Question({ question, onAnswered }) {
     <>
       <h1>Question {id}</h1>
       <h3>{prompt}</h3>
+      {/* answers is an array that holds the option of possible answers  */}
       {answers.map((answer, index) => {
+        // a value for isCorrect will be set if the index equals the correctIndex, if this value
+        // is sent, the score will go up, if no value saved, the score will not go up
         const isCorrect = index === correctIndex;
         return (
           <button key={answer} onClick={() => handleAnswer(isCorrect)}>
